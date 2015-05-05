1a5182606d592c282c368ea2b54493531577a113
Renette Ros <renetteros@gmail.com>
2015-05-05 23:59:06 +0200

    Added missing elements to detail page.

--- detailedResult.php
+++ detailedResult.php
@@ -1,8 +1,8 @@
 <div id="information">
     <?php $apiKey = "AIzaSyDk9GnS9P2LscHZBJ5AdsgSB_mmQDfTEr8";
-         $id = $_GET['Name'];
+    $id = $_GET['Name'];
 
-        $result = mysqli_query($conn, "SELECT * FROM restaurants
+    $result = mysqli_query($conn, "SELECT * FROM restaurants
           INNER JOIN venue_link on venue_link.restaurant = restaurant_id
           INNER JOIN food_link on food_link.restaurant = restaurant_id
           INNER JOIN prices on price_id = Price
@@ -10,34 +10,58 @@
           WHERE restaurant_id = $id
           GROUP BY restaurant_id");
 
-        $ans = mysqli_fetch_assoc($result);
-        $src = "https://www.google.com/maps/embed/v1/place?key=".$apiKey."&q=".urlencode($ans["Name"].",".$ans['address']);
+    $ans = mysqli_fetch_assoc($result);
+    $src = "https://www.google.com/maps/embed/v1/place?key=".$apiKey."&q=".urlencode($ans["Name"].",".$ans['address']);
     ?>
 
-	<div id="googleMap">
-        <?php echo("<iframe src='$src'></iframe>"); ?>
-	</div>
+    <?php echo("<iframe id='googleMap' src='$src'></iframe>"); ?>
 
 
     <?php
-        $value = $ans;
+    $value = $ans;
 
-	 	echo("<img class='logo' width='150' height='150' src='logos/".$value['logoPath']."'' alt='Restaurant Logo' />") ?>
-		<span class="rest-name">Name: <?php echo($value['Name']); ?></span><br>
-		 <span class="dresscode">Dresscode: <?php echo($value['Dresscode']); ?></span><br>
-		<?php if(!empty($value['Website'])) echo("<a href='".$value['Website']."'>".$value['Website'] ."</a><br>"); ?>
-		<?php if(!empty($value['Facebook'])) echo("<a href='".$value['Facebook']."'>".$value['Facebook']."</a><br>"); ?>
-		<?php if(!empty($value['Twitter'])) echo("<a href='".$value['Twitter']."'>".$value['Twitter']."</a><br>"); ?>
-		<?php if(!empty($value['Youtube'])) echo("<a href='".$value['Youtube']."'>".$value['Youtube']."</a><br>"); ?>
-		<span class="price">Price Range: <?php echo($value['price_category']) ?></span><br>
-		<?php
+    echo("<img class='logo' width='150' height='150' src='logos/".$value['logoPath']."'' alt='Restaurant Logo' />")
+    ?>
+    <div class="rest-name"><span class="label">Name</span> <?php echo($value['Name']); ?> </div>
+    <div class="dresscode"><span class="label">Dress code</span> <?php echo($value['Dresscode']); ?></div>
+    <?php if(!empty($value['telephone'])) {?>
+        <div class="address"><span class="label">Phone</span>  <?php echo($value['telephone']); ?></div>
+    <?php } ?>
+    <div class="address"><span class="label">Address</span>  <?php echo($value['address']); ?></div>
+    <div class="online">
+        <?php if(!empty($value['Website'])) echo("<a href='".$value['Website']."'>".$value['Website'] ."</a><br>"); ?>
+        <?php if(!empty($value['Facebook'])) echo("<a href='".$value['Facebook']."'>".$value['Facebook']."</a><br>"); ?>
+        <?php if(!empty($value['Twitter'])) echo("<a href='".$value['Twitter']."'>".$value['Twitter']."</a><br>"); ?>
+        <?php if(!empty($value['Youtube'])) echo("<a href='".$value['Youtube']."'>".$value['Youtube']."</a><br>"); ?>
+    </div>
+    <div class="price"><span class="label">Price Range</span> <?php echo($value['price_category']) ?></div>
 
-		//TODO - Query for food type
-		//TODO - Query for adresses
-		//TODO - Query for venue - type
+    <div class="food-types">
+        <span class="label">Food Categories</span>
+    <?php
+    $result = mysqli_query($conn, "SELECT DISTINCT food_category FROM food_categories
+          INNER JOIN  food_link ON food_type = food_categories.food_id
+          WHERE restaurant = $ans[restaurant_id]");
+    while($row = mysqli_fetch_assoc($result))
+    {
+       echo("<span class='food'> $row[food_category]</span>");
+    }
+    ?>
+    </div>
 
-            ?>
-		<span class="price">Food Category: <?php echo($value['food_category']) ?></span><br>
+    <div class="venue-types">
+        <span class="label">Venue</span>
+        <?php
+
+        $result = mysqli_query($conn, "SELECT DISTINCT venue_name FROM venue_categories
+          INNER JOIN  venue_link ON venue_type = venue_categories.venue_id
+          WHERE restaurant = $ans[restaurant_id]");
+        while($row = mysqli_fetch_assoc($result))
+        {
+            echo("<span class='venue'> $row[venue_name]</span>");
+        }
+        ?>
+    </div>
 
 </div>
 <input type='button' value='Back' id='backPrev'>
