dec0f9462995779fa2ba1d0bb7300e40be7988c5
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-05-18 17:02:44 +0200

    Removed BaconFinder from the results page

--- results.php
+++ results.php
@@ -2,11 +2,8 @@
 $PageTitle = "Search Results" ;
 include "partials/menu.php";
 ?>
-		<div id="BaconFinder">
-			<?php include "partials/bacon-finder.php"; ?>
-		</div>
-		<div id="content">
-            <!--button id="back">Back</button-->
-			<?php include "partials/results-frame.php"; ?>
-		</div>
+	<div id="content">
+        <!--button id="back">Back</button-->
+		<?php include "partials/results-frame.php"; ?>
+	</div>
 <?php include "partials/footer.php" ?>
