30f057dfa5375ca729257800634eab8a99016fee
Elzahn Botha <elzahnbotha@mweb.co.za>
2015-05-18 18:14:58 +0200

    Added my part to the About page

--- about.php
+++ about.php
@@ -4,10 +4,10 @@ $PageTitle = "About Us";
 ?>
 <div id="aboutContent">
 	<div class="member">
-		<img src="images/dummyMember.png" alt="member image" class="memberImage">
+		<img src="images/ElzahnBotha.jpg" alt="member image" class="memberImage">
 		<p class="member">
-			 Elzahn <br/>
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			Elzahn <br/>
+			"If I could afford takeout every night, Bacon finder would definitely be my new best friend."
 		</p>
 	</div>
 	<div class="member">
