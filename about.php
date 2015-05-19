612be10bdf7a77d464f2069de88ea75972f64471
Renette Ros <renetteros@gmail.com>
2015-05-19 18:49:30 +0200

    Added my picture and quote

--- about.php
+++ about.php
@@ -7,28 +7,28 @@ $PageTitle = "About Us";
 		<img src="images/ElzahnBotha.jpg" alt="member image" class="memberImage">
 		<p class="member">
 			Elzahn <br/>
-			"If I could afford takeout every night, Bacon finder would definitely be my new best friend."
+			<q>If I could afford takeout every night, Bacon finder would definitely be my new best friend.</q>
 		</p>
 	</div>
 	<div class="member">
 		<img src="images/dummyMember.png" alt="member image" class="memberImage">
 		<p class="member">
 			 Frank <br/>
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			<q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</q>
 		</p>
 	</div>
 	<div class="member">
-		<img src="images/dummyMember.png" alt="member image" class="memberImage">
+		<img src="images/Renette.JPG" alt="member image" class="memberImage">
 		<p class="member">
 			 Renette<br/>
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			<q>Its food. Close to you. What more needs to be said?</q>
 		</p>
 	</div>
 	<div class="member">
 		<img src="images/tania.jpg" alt="member image" class="memberImage">
 		<p class="member">
 			 Tania<br/>
-			"I think Bacon Finder is a great way for new students, who are lazy to cook or don't really have time for it, to find a variety of food sources in and around Hatfield."
+			<q>I think Bacon Finder is a great way for new students, who are lazy to cook or don't really have time for it, to find a variety of food sources in and around Hatfield.</q>
 		</p>
 	</div>
 </div>
