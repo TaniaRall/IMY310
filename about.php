b4ac99cad1f8fd4f385fad8175d355904262df7b
Frank <frank.jvr.s3@gmail.com>
2015-05-20 19:13:26 +0200

    Updated about
    Put in my description and added member surnames

--- about.php
+++ about.php
@@ -6,28 +6,28 @@ $PageTitle = "About Us";
 	<div class="member">
 		<img src="images/ElzahnBotha.jpg" alt="member image" class="memberImage">
 		<p class="member">
-			Elzahn <br/>
+			Elzahn Botha<br/>
 			<q>If I could afford takeout every night, Bacon finder would definitely be my new best friend.</q>
 		</p>
 	</div>
 	<div class="member">
-		<img src="images/dummyMember.png" alt="member image" class="memberImage">
+		<img src="images/Frank.jpg" alt="member image" class="memberImage">
 		<p class="member">
-			 Frank <br/>
-			<q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</q>
+			 Frank Janse van Rensburg<br/>
+			<q>I moustache you a question. Do you like food as much as me?</q>
 		</p>
 	</div>
 	<div class="member">
 		<img src="images/Renette.JPG" alt="member image" class="memberImage">
 		<p class="member">
-			 Renette<br/>
+			 Renette Ross<br/>
 			<q>It's food. Close to you. What more needs to be said?</q>
 		</p>
 	</div>
 	<div class="member">
 		<img src="images/tania.jpg" alt="member image" class="memberImage">
 		<p class="member">
-			 Tania<br/>
+			 Tania Rall<br/>
 			<q>I think Bacon Finder is a great way for new students, who are lazy to cook or don't really have time for it, to find a variety of food sources in and around Hatfield.</q>
 		</p>
 	</div>
